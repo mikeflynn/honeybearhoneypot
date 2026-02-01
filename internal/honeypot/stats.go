918ee33471a23c486ad5dd0a4a4fd16396543369
Mike Flynn <mike@thatmikeflynn.com>
2026-01-31 16:42:23 -0800

    Refactored GUI to prevent thread blocking.

--- internal/honeypot/stats.go
+++ internal/honeypot/stats.go
@@ -1,6 +1,7 @@
 package honeypot
 
 import (
+	"sync"
 	"time"
 
 	"github.com/charmbracelet/log"
@@ -10,6 +11,8 @@ import (
 var (
 	usersAllTimeCacheValue int
 	usersAllTimeCacheTTL   time.Time
+	usersAllTimeMu         sync.Mutex
+	usersAllTimeUpdating   bool
 )
 
 func StatActiveUsers() int {
@@ -21,32 +24,55 @@ func StatUsersThisSession() int {
 }
 
 func StatUsersAllTime() int {
+	usersAllTimeMu.Lock()
+	val := usersAllTimeCacheValue
+	ttl := usersAllTimeCacheTTL
+	updating := usersAllTimeUpdating
+	usersAllTimeMu.Unlock()
+
 	now := time.Now()
-	if now.Before(usersAllTimeCacheTTL) {
-		return usersAllTimeCacheValue
+	if now.After(ttl) && !updating {
+		usersAllTimeMu.Lock()
+		usersAllTimeUpdating = true
+		usersAllTimeMu.Unlock()
+
+		go func() {
+			defer func() {
+				usersAllTimeMu.Lock()
+				usersAllTimeUpdating = false
+				usersAllTimeMu.Unlock()
+			}()
+
+			data, err := entity.EventCountQuery(`
+				SELECT
+					"logins" AS Value,
+					COUNT(*) AS Count
+				FROM events
+				WHERE
+					events.type = 'login'
+					AND events.source = 'user'
+			`)
+
+			usersAllTimeMu.Lock()
+			defer usersAllTimeMu.Unlock()
+
+			if err != nil {
+				log.Error("statUsersAllTime", "error", err)
+				// extend TTL slightly on error to avoid rapid retry
+				usersAllTimeCacheTTL = time.Now().Add(5 * time.Second)
+				return
+			}
+
+			if len(data) == 0 {
+				usersAllTimeCacheValue = 0
+			} else {
+				usersAllTimeCacheValue = data[0].Count
+			}
+			usersAllTimeCacheTTL = time.Now().Add(10 * time.Second)
+		}()
 	}
 
-	data, err := entity.EventCountQuery(`
-		SELECT
-			"logins" AS Value,
-			COUNT(*) AS Count
-		FROM events
-		WHERE
-			events.type = 'login'
-			AND events.source = 'user'
-	`)
-	if err != nil {
-		log.Error("statUsersAllTime", "error", err)
-		return usersAllTimeCacheValue // fallback to last cached value
-	}
-
-	if len(data) == 0 {
-		usersAllTimeCacheValue = 0
-	} else {
-		usersAllTimeCacheValue = data[0].Count
-	}
-	usersAllTimeCacheTTL = now.Add(10 * time.Second)
-	return usersAllTimeCacheValue
+	return val
 }
 
 func StatMaxUsers() int {
